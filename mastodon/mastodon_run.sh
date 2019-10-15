e38bf3a3216036fa8dfb9dcac23cb5cb17e5b6c4
kim (grufwub) <grufwub@gmail.com>
2019-10-15 09:20:41 +0100

    update mastodon_run script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- mastodon/mastodon_run.sh
+++ mastodon/mastodon_run.sh
@@ -6,27 +6,31 @@ RUN="$HOME/run"
 
 start_mastodon_service() {
   local name=$1
-  shift
+  local environment=$2
+  shift 2
 
-  if [ ! -f "$RUN/mastodon-$name.pid" ]; then
+  if ( ! status_mastodon_service $name > /dev/null 2>&1 ); then
+    if [ -f "$RUN/mastodon-$name.pid" ]; then
+      echo "Unused mastodon-$name.pid found, deleting..."
+      rm $RUN/mastodon-$name.pid
+    fi
+
     echo "Starting mastodon-$name"
-    $@ > $RUN/mastodon-$name.log 2>&1 &
+    env $environment $@ > $RUN/mastodon-$name.log 2>&1 &
     echo "$!" > $RUN/mastodon-$name.pid
-  else
-    echo "mastodon-$name PID file already exists"
   fi
 }
 
 start_mastodon_web() {
-  start_mastodon_service 'web' env MAX_TOOT_CHARS=6942 RAILS_ENV=production PORT=3000 /home/mastodon/.rbenv/shims/bundle exec puma -C config/puma.rb
+  start_mastodon_service 'web' 'MAX_TOOT_CHARS=6942 RAILS_ENV=production PORT=3000' /home/mastodon/.rbenv/shims/bundle exec puma -C config/puma.rb
 }
 
 start_mastodon_sidekiq() {
-  start_mastodon_service 'sidekiq' env RAILS_ENV=production DB_POOL=25 MALLOC_ARENA_MAX=2 /home/mastodon/.rbenv/shims/bundle exec sidekiq -c 25
+  start_mastodon_service 'sidekiq' 'RAILS_ENV=production DB_POOL=25 MALLOC_ARENA_MAX=2' /home/mastodon/.rbenv/shims/bundle exec sidekiq -c 25
 }
 
 start_mastodon_streaming() {
-  start_mastodon_service 'streaming' env MAX_TOOT_CHARS=6942 RAILS_ENV=production PORT=4000 STREAMING_CLUSTER_NUM=1 /usr/bin/node ./streaming
+  start_mastodon_service 'streaming' 'MAX_TOOT_CHARS=6942 RAILS_ENV=production PORT=4000 STREAMING_CLUSTER_NUM=1' /usr/bin/node ./streaming
 }
 
 stop_mastodon_service() {
@@ -94,25 +98,29 @@ stop_mastodon() {
 }
 
 status_services() {
+  local result=0
+
   # sudo check
   sudo -v || return 1
 
   echo -n 'nginx -- '
-  sudo service nginx status
+  sudo service nginx status || result=1
 
   echo -n 'redis-server -- '
-  sudo service redis-server status
+  sudo service redis-server status || result=1
 
   echo -n 'postgresql -- '
-  sudo service postgresql status
+  sudo service postgresql status || result=1
+
+  return $result
 }
 
 status_mastodon_service() {
-  if [ -f "$RUN/mastodon-$1.pid" ] && ( cat $RUN/mastodon-$1.pid | grep -E '^[0-9]+$' > /dev/null 2>&1 ); then
-    [ "$2" = '--silent' ] && echo "mastodon-$1 -- running (PID: $(cat $RUN/mastodon-$1.pid))"
+  if [ -f "$RUN/mastodon-$1.pid" ] && ( cat $RUN/mastodon-$1.pid | grep -E '^[0-9]+$' > /dev/null 2>&1 ) && ( ! ps au | grep -F "$@" | grep -Fq "$(cat $RUN/mastodon-$1.pid)"); then
+    echo "mastodon-$1 -- running (PID: $(cat $RUN/mastodon-$1.pid))"
     return 0
   else
-    [ "$2" != '--silent' ] && echo "mastodon-$1 -- not running!"
+    echo "mastodon-$1 -- not running!"
     return 1
   fi
 }
@@ -136,15 +144,21 @@ if [ ! -d "$RUN" ]; then
 fi
 
 case "$1" in
-  'start')
+  'start-services')
+    echo 'STARTING REQUIRED SERVICES...'
+    start_services || return $?
+    echo 'STARTED!'
+    ;;
+
+  'start-mastodon')
     echo 'STARTING MASTODON...'
-
-    if [ "$2" = '--all' ]; then
-      start_services || return $?
+    if ( ! status_services > /dev/null 2>&1 ); then
+      echo 'Required services are not running, starting!'
+      start-services
+      return 1
     fi
-
     start_mastodon || return $?
-    echo 'STARTED'
+    echo 'STARTED!'
     ;;
 
   'stop')
