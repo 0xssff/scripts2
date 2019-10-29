05f595fa5e499de7e426c21611c6be08253e6091
kim (grufwub) <grufwub@gmail.com>
2019-10-29 14:46:18 +0000

    add more default shim scripts
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

---
+++ shim/scripts/list.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+USER_DIR="$HOME/.user"
+USER_SCRIPTS_DIR="$USER_DIR/scripts"
+
+ls --color=auto "$USER_SCRIPTS_DIR"
--- shim/scripts/remove.sh
+++ shim/scripts/remove.sh
@@ -6,9 +6,6 @@ USER_SCRIPTS_DIR="$USER_DIR/scripts"
 if ( echo "$PWD" | grep -q -e "^$USER_DIR" ); then
   printf "aaaaaaa please don't use this script under the $USER_DIR directory, things might break!\n"
   return 1
-elif [ "$1" = '' ] || [ $# -gt 1 ]; then
-  printf "invalid arguments supplied!\n"
-  return 1
 fi
 
-cp "$1" "$USER_SCRIPTS_DIR" > /dev/null 2>&1 || printf "Failed to copy $1 to $USER_SCRIPTS_DIR\n"
+rm "$USER_SCRIPTS_DIR/$1" > /dev/null 2>&1 || printf "Failed to remove $1 from $USER_SCRIPTS_DIR\n"
