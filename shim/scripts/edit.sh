53d9ed5a26231da6595c98fee2820cce9ccb06bb
kim (grufwub) <grufwub@gmail.com>
2019-11-05 22:04:49 +0000

    add shim edit.sh script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- shim/scripts/edit.sh
+++ shim/scripts/edit.sh
@@ -3,4 +3,9 @@
 USER_DIR="$HOME/.user"
 USER_SCRIPTS_DIR="$USER_DIR/scripts"
 
+if [ $# -eq 0 ]; then
+  printf 'please supply a filename to edit!\n'
+  return 1
+fi
+
 $EDITOR "$USER_SCRIPTS_DIR/$1"
